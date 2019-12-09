d59660d7242f4335a9e5727ea814a312be89071e
haticeSigirci <hatice.sigirci@hotmail.com>
2019-12-09 21:24:12 +0100

    init

---
+++ spring-netflix-eureka-client/src/main/java/com/hkumru/springnetflixeurekaclient/AccountController.java
@@ -0,0 +1,10 @@
+package com.hkumru.springnetflixeurekaclient;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public interface AccountController {
+
+
+}
